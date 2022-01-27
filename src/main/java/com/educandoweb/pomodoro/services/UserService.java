8967984dd980c60375401f6ccfcfc68d46b72810
Aline Osmar <aliliosmar@gmail.com>
2022-01-27 12:17:52 -0300

    exception handling - findById

--- src/main/java/com/educandoweb/pomodoro/services/UserService.java
+++ src/main/java/com/educandoweb/pomodoro/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.pomodoro.entities.User;
 import com.educandoweb.pomodoro.repositories.UserRepository;
+import com.educandoweb.pomodoro.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
     public User findById(Long id)
     {
     	Optional<User> obj = repository.findById(id);
-    	return obj.get();
+    	return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj)
     {
