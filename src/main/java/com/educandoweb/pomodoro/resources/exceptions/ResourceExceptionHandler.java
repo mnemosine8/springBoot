d0ddc6818a535d0bed96c894eb0133e59b2cccd0
Aline Osmar <aliliosmar@gmail.com>
2022-01-27 12:37:47 -0300

    exception handling - delete

--- src/main/java/com/educandoweb/pomodoro/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/pomodoro/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.pomodoro.services.exceptions.DatabaseException;
 import com.educandoweb.pomodoro.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -20,4 +21,15 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());	
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError>database(DatabaseException e, HttpServletRequest request)
+	{
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());	
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
 }
--- src/main/java/com/educandoweb/pomodoro/services/UserService.java
+++ src/main/java/com/educandoweb/pomodoro/services/UserService.java
@@ -3,10 +3,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.pomodoro.entities.User;
 import com.educandoweb.pomodoro.repositories.UserRepository;
+import com.educandoweb.pomodoro.services.exceptions.DatabaseException;
 import com.educandoweb.pomodoro.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,9 +32,22 @@ public class UserService {
     	return repository.save(obj);
     }
     
+    
     public void delete(Long id)
     {
-    	repository.deleteById(id);
+    	try {
+    		repository.deleteById(id);
+    		
+    	}catch(EmptyResultDataAccessException e)
+    	{
+    		throw new ResourceNotFoundException(id);
+    		
+    	}
+    	catch(DataIntegrityViolationException e)
+    	{
+    		throw new DatabaseException(e.getMessage());
+    	}
+    	
     }
     public User update(Long id, User obj)
     {
