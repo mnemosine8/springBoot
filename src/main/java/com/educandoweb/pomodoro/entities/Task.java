dba99315bb2411b97c477203dbfd4d36a355d43e
Aline Osmar <aline.machado.osmar@hp.com>
2022-03-25 11:48:32 -0300

    b

--- src/main/java/com/educandoweb/pomodoro/entities/Task.java
+++ src/main/java/com/educandoweb/pomodoro/entities/Task.java
@@ -60,11 +60,11 @@ public class Task implements Serializable {
 		return true;
 	}
 
-	public Long getid() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setid(Long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
